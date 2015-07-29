8a896283b622b930f6f1d2d78705582347441c85
wdlin <460799951@qq.com>
2015-07-29 14:17:43 +0800

    change

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,8 +1,8 @@
-#include "MainWindow.h"
+﻿#include "MainWindow.h"
 #include "ui_mainwindow.h"
 #include "SerialPortThread.h"
 #include <QMessageBox>
-//
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),isEnterSend(true),isAutoEnter(true),historyIterator(inputHistory.end()),
     ui(new Ui::MainWindow)
